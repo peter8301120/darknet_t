8dae420895d7cc43d158b52789090b35ccf1f1d8
Tinway <z4573333@gmail.com>
2020-04-10 12:05:54 +0800

    revise ftp

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@ HOST=192.168.2.102
 USERNAME=myuser
 PASSWORD=abvc123456
 lftp myuser:abvc123456@192.168.2.102 <<EOF  
-	lcd ./ftp-download
+	lcd ./ftp-download01
 	cd /Image/Download01
 	cd `date +"%Y%m%d%H"`
 	mget *.jpg
@@ -11,6 +11,7 @@ lftp myuser:abvc123456@192.168.2.102 <<EOF
 	cd ..
 	cd ./Download02
 	cd `date +"%Y%m%d%H"`
+	lcd ./ftp-download02
 	mget *.jpg
 	mget *.JPG
 	bye
--- s.sh
+++ s.sh
@@ -12,21 +12,21 @@ do
 	#echo $time2
 
 	#5分下載前一小時資料
-	if [ "$time1" = "$time2" ] && [ "$time_m" = 05 ]
-	then
-		sh download.sh
-		time2=$((time1+1))
-		echo $time2
-	else
-		echo "Wait for 3 seconds..."
-		sleep 3
-		echo $time_m
-	fi
+	#if [ "$time1" = "$time2" ] && [ "$time_m" = 05 ]
+	#then
+	sh download.sh
+	time2=$((time1+1))
+	echo $time2
+	#else
+	echo "Wait for 3 seconds..."
+	sleep 3
+	echo $time_m
+	#fi
 	#55分上傳成果txt檔
-	if [ "$time_m" = 55 ]
-	then
-		sh upload.sh
-		echo $time_m
-	fi
+	#if [ "$time_m" = 55 ]
+	#then
+	#	sh upload.sh
+	#	echo $time_m
+	#fi
 
 done
